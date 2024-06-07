redpanda-data/benthos

Add more cli customisation options

internal/cli/common/opts.go
@@ -22,6 +22,9 @@ type CLIOpts struct {
 	ProductName      string
 	DocumentationURL string
 
+	ShowRunCommand    bool
+	ConfigSearchPaths []string
+
 	MainConfigSpecCtor   func() docs.FieldSpecs // TODO: This becomes a service.Environment
 	OnManagerInitialised func(mgr bundle.NewManagement, pConf *docs.ParsedConfig) error
 	OnLoggerInit         func(l log.Modular) (log.Modular, error)
@@ -33,11 +36,17 @@ func NewCLIOpts(version, dateBuilt string) *CLIOpts {
 		binaryName = path.Base(os.Args[0])
 	}
 	return &CLIOpts{
-		Version:            version,
-		DateBuilt:          dateBuilt,
-		BinaryName:         binaryName,
-		ProductName:        "Benthos",
-		DocumentationURL:   "https://benthos.dev/docs",
+		Version:          version,
+		DateBuilt:        dateBuilt,
+		BinaryName:       binaryName,
+		ProductName:      "Benthos",
+		DocumentationURL: "https://benthos.dev/docs",
+		ShowRunCommand:   false,
+		ConfigSearchPaths: []string{
+			"/benthos.yaml",
+			"/etc/benthos/config.yaml",
+			"/etc/benthos.yaml",
+		},
 		MainConfigSpecCtor: config.Spec,
 		OnManagerInitialised: func(mgr bundle.NewManagement, pConf *docs.ParsedConfig) error {
 			return nil

gregfurman/bento

internal/cli/common/opts.go
@@ -22,6 +22,9 @@ type CLIOpts struct {
 	ProductName      string
 	DocumentationURL string
 
+	ShowRunCommand    bool
+	ConfigSearchPaths []string
+
 	MainConfigSpecCtor   func() docs.FieldSpecs // TODO: This becomes a service.Environment
 	OnManagerInitialised func(mgr bundle.NewManagement, pConf *docs.ParsedConfig) error
 	OnLoggerInit         func(l log.Modular) (log.Modular, error)
@@ -33,11 +36,17 @@ func NewCLIOpts(version, dateBuilt string) *CLIOpts {
 		binaryName = path.Base(os.Args[0])
 	}
 	return &CLIOpts{
-		Version:            version,
-		DateBuilt:          dateBuilt,
-		BinaryName:         binaryName,
-		ProductName:        "Bento",
-		DocumentationURL:   "https://warpstreamlabs.github.io/bento/docs",
+		Version:          version,
+		DateBuilt:        dateBuilt,
+		BinaryName:       binaryName,
+		ProductName:      "Bento",
+		DocumentationURL: "https://warpstreamlabs.github.io/bento/docs",
+		ShowRunCommand:   false,
+		ConfigSearchPaths: []string{
+			"/benthos.yaml",
+			"/etc/benthos/config.yaml",
+			"/etc/benthos.yaml",
+		},
 		MainConfigSpecCtor: config.Spec,
 		OnManagerInitialised: func(mgr bundle.NewManagement, pConf *docs.ParsedConfig) error {
 			return nil