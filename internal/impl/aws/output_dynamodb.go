cyborginc/connect

Convert docs to asciidoc

internal/impl/aws/output_dynamodb.go
@@ -74,7 +74,7 @@ func ddboOutputSpec() *service.ConfigSpec {
 		Categories("Services", "AWS").
 		Summary(`Inserts items into a DynamoDB table.`).
 		Description(`
-The field `+"`string_columns`"+` is a map of column names to string values, where the values are [function interpolated](/docs/configuration/interpolation#bloblang-queries) per message of a batch. This allows you to populate string columns of an item by extracting fields within the document payload or metadata like follows:
+The field `+"`string_columns`"+` is a map of column names to string values, where the values are xref:configuration:interpolation.adoc#bloblang-queries[function interpolated] per message of a batch. This allows you to populate string columns of an item by extracting fields within the document payload or metadata like follows:
 
 `+"```yml"+`
 string_columns:
@@ -84,7 +84,7 @@ string_columns:
   full_content: ${!content()}
 `+"```"+`
 
-The field `+"`json_map_columns`"+` is a map of column names to json paths, where the [dot path](/docs/configuration/field_paths) is extracted from each document and converted into a map value. Both an empty path and the path `+"`.`"+` are interpreted as the root of the document. This allows you to populate map columns of an item like follows:
+The field `+"`json_map_columns`"+` is a map of column names to json paths, where the xref:configuration:field_paths.adoc[dot path] is extracted from each document and converted into a map value. Both an empty path and the path `+"`.`"+` are interpreted as the root of the document. This allows you to populate map columns of an item like follows:
 
 `+"```yml"+`
 json_map_columns:
@@ -101,15 +101,15 @@ json_map_columns:
 
 In which case the top level document fields will be written at the root of the item, potentially overwriting previously defined column values. If a path is not found within a document the column will not be populated.
 
-### Credentials
+== Credentials
 
-By default Benthos will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more [in this document](/docs/guides/cloud/aws).
+By default Benthos will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more in xref:guides:cloud/aws.adoc[].
 
-## Performance
+== Performance
 
 This output benefits from sending multiple messages in flight in parallel for improved performance. You can tune the max number of in flight messages (or message batches) with the field `+"`max_in_flight`"+`.
 
-This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more [in this doc](/docs/configuration/batching).
+This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more xref:configuration:batching.adoc[in this doc].
 `).
 		Fields(
 			service.NewStringField(ddboFieldTable).
@@ -124,7 +124,7 @@ This output benefits from sending messages as a batch for improved performance. 
 					"full_content": "${!content()}",
 				}),
 			service.NewStringMapField(ddboFieldJSONMapColumns).
-				Description("A map of column keys to [field paths](/docs/configuration/field_paths) pointing to value data within messages.").
+				Description("A map of column keys to xref:configuration:field_paths.adoc[field paths] pointing to value data within messages.").
 				Default(map[string]any{}).
 				Example(map[string]any{
 					"user":           "path.to.user",

internal/impl/aws/output_kinesis_firehose.go
@@ -48,15 +48,15 @@ func kfoOutputSpec() *service.ConfigSpec {
 		Categories("Services", "AWS").
 		Summary(`Sends messages to a Kinesis Firehose delivery stream.`).
 		Description(`
-### Credentials
+== Credentials
 
-By default Benthos will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more [in this document](/docs/guides/cloud/aws).
+By default Benthos will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more in xref:guides:cloud/aws.adoc[].
 
-## Performance
+== Performance
 
 This output benefits from sending multiple messages in flight in parallel for improved performance. You can tune the max number of in flight messages (or message batches) with the field `+"`max_in_flight`"+`.
 
-This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more [in this doc](/docs/configuration/batching).
+This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more xref:configuration:batching.adoc[in this doc].
 `).
 		Fields(
 			service.NewStringField(kfoFieldStream).

internal/impl/cockroachdb/input_changefeed.go
@@ -30,8 +30,8 @@ var sampleString = `{
 func crdbChangefeedInputConfig() *service.ConfigSpec {
 	return service.NewConfigSpec().
 		Categories("Integration").
-		Summary(fmt.Sprintf("Listens to a [CockroachDB Core Changefeed](https://www.cockroachlabs.com/docs/stable/changefeed-examples) and creates a message for each row received. Each message is a json object looking like: \n```json\n%s\n```", sampleString)).
-		Description("This input will continue to listen to the changefeed until shutdown. A backfill of the full current state of the table will be delivered upon each run unless a cache is configured for storing cursor timestamps, as this is how Benthos keeps track as to which changes have been successfully delivered.\n\nNote: You must have `SET CLUSTER SETTING kv.rangefeed.enabled = true;` on your CRDB cluster, for more information refer to [the official CockroachDB documentation.](https://www.cockroachlabs.com/docs/stable/changefeed-examples?filters=core)").
+		Summary(fmt.Sprintf("Listens to a https://www.cockroachlabs.com/docs/stable/changefeed-examples[CockroachDB Core Changefeed] and creates a message for each row received. Each message is a json object looking like: \n```json\n%s\n```", sampleString)).
+		Description("This input will continue to listen to the changefeed until shutdown. A backfill of the full current state of the table will be delivered upon each run unless a cache is configured for storing cursor timestamps, as this is how Benthos keeps track as to which changes have been successfully delivered.\n\nNote: You must have `SET CLUSTER SETTING kv.rangefeed.enabled = true;` on your CRDB cluster, for more information refer to https://www.cockroachlabs.com/docs/stable/changefeed-examples?filters=core[the official CockroachDB documentation].").
 		Fields(
 			service.NewStringField("dsn").
 				Description(`A Data Source Name to identify the target database.`).
@@ -41,7 +41,7 @@ func crdbChangefeedInputConfig() *service.ConfigSpec {
 				Description("CSV of tables to be included in the changefeed").
 				Example([]string{"table1", "table2"}),
 			service.NewStringField("cursor_cache").
-				Description("A [cache resource](https://www.benthos.dev/docs/components/caches/about) to use for storing the current latest cursor that has been successfully delivered, this allows Benthos to continue from that cursor upon restart, rather than consume the entire state of the table.").
+				Description("A https://www.docs.redpanda.com/redpanda-connect/components/caches/about[cache resource] to use for storing the current latest cursor that has been successfully delivered, this allows Benthos to continue from that cursor upon restart, rather than consume the entire state of the table.").
 				Optional(),
 			service.NewStringListField("options").
 				Description("A list of options to be included in the changefeed (WITH X, Y...).\n**NOTE: Both the CURSOR option and UPDATED will be ignored from these options when a `cursor_cache` is specified, as they are set explicitly by Benthos in this case.**").

internal/impl/pure/processor_metric.go
@@ -31,18 +31,18 @@ func metProcSpec() *service.ConfigSpec {
 		Stable().
 		Summary("Emit custom metrics by extracting values from messages.").
 		Description(`
-This processor works by evaluating an [interpolated field `+"`value`"+`](/docs/configuration/interpolation#bloblang-queries) for each message and updating a emitted metric according to the [type](#types).
+This processor works by evaluating an xref:configuration:interpolation.adoc#bloblang-queries[interpolated field `+"`value`"+`] for each message and updating a emitted metric according to the <<types, type>>.
 
-Custom metrics such as these are emitted along with Benthos internal metrics, where you can customize where metrics are sent, which metric names are emitted and rename them as/when appropriate. For more information check out the [metrics docs here](/docs/components/metrics/about).`).
+Custom metrics such as these are emitted along with Benthos internal metrics, where you can customize where metrics are sent, which metric names are emitted and rename them as/when appropriate. For more information see the xref:components:metrics/about.adoc[metrics docs].`).
 		Footnotes(`
-## Types
+== Types
 
-### `+"`counter`"+`
+=== `+"`counter`"+`
 
 Increments a counter by exactly 1, the contents of `+"`value`"+` are ignored
 by this type.
 
-### `+"`counter_by`"+`
+=== `+"`counter_by`"+`
 
 If the contents of `+"`value`"+` can be parsed as a positive integer value
 then the counter is incremented by this value.
@@ -59,7 +59,7 @@ pipeline:
         value: ${!json("field.some.value")}
 `+"```"+`
 
-### `+"`gauge`"+`
+=== `+"`gauge`"+`
 
 If the contents of `+"`value`"+` can be parsed as a positive integer value
 then the gauge is set to this value.
@@ -76,7 +76,7 @@ pipeline:
         value: ${!json("field.some.value")}
 `+"```"+`
 
-### `+"`timing`"+`
+=== `+"`timing`"+`
 
 Equivalent to `+"`gauge`"+` where instead the metric is a timing. It is recommended that timing values are recorded in nanoseconds in order to be consistent with standard Benthos timing metrics, as in some cases these values are automatically converted into other units such as when exporting timings as histograms with Prometheus metrics.`).
 		Example(
@@ -124,7 +124,7 @@ metrics:
 		).
 		Fields(
 			service.NewStringEnumField(metProcFieldType, "counter", "counter_by", "gauge", "timing").
-				Description("The metric [type](#types) to create."),
+				Description("The metric <<types, type>> to create."),
 			service.NewStringField(metProcFieldName).
 				Description("The name of the metric to create, this must be unique across all Benthos components otherwise it will overwrite those other metrics."),
 			service.NewInterpolatedStringMapField(metProcFieldLabels).

gregfurman/bento

internal/impl/aws/output_dynamodb.go
@@ -74,7 +74,7 @@ func ddboOutputSpec() *service.ConfigSpec {
 		Categories("Services", "AWS").
 		Summary(`Inserts items into a DynamoDB table.`).
 		Description(`
-The field `+"`string_columns`"+` is a map of column names to string values, where the values are [function interpolated](/docs/configuration/interpolation#bloblang-queries) per message of a batch. This allows you to populate string columns of an item by extracting fields within the document payload or metadata like follows:
+The field `+"`string_columns`"+` is a map of column names to string values, where the values are xref:configuration:interpolation.adoc#bloblang-queries[function interpolated] per message of a batch. This allows you to populate string columns of an item by extracting fields within the document payload or metadata like follows:
 
 `+"```yml"+`
 string_columns:
@@ -84,7 +84,7 @@ string_columns:
   full_content: ${!content()}
 `+"```"+`
 
-The field `+"`json_map_columns`"+` is a map of column names to json paths, where the [dot path](/docs/configuration/field_paths) is extracted from each document and converted into a map value. Both an empty path and the path `+"`.`"+` are interpreted as the root of the document. This allows you to populate map columns of an item like follows:
+The field `+"`json_map_columns`"+` is a map of column names to json paths, where the xref:configuration:field_paths.adoc[dot path] is extracted from each document and converted into a map value. Both an empty path and the path `+"`.`"+` are interpreted as the root of the document. This allows you to populate map columns of an item like follows:
 
 `+"```yml"+`
 json_map_columns:
@@ -101,15 +101,15 @@ json_map_columns:
 
 In which case the top level document fields will be written at the root of the item, potentially overwriting previously defined column values. If a path is not found within a document the column will not be populated.
 
-### Credentials
+== Credentials
 
-By default Bento will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more [in this document](/docs/guides/cloud/aws).
+By default Bento will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more in xref:guides:cloud/aws.adoc[].
 
-## Performance
+== Performance
 
 This output benefits from sending multiple messages in flight in parallel for improved performance. You can tune the max number of in flight messages (or message batches) with the field `+"`max_in_flight`"+`.
 
-This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more [in this doc](/docs/configuration/batching).
+This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more xref:configuration:batching.adoc[in this doc].
 `).
 		Fields(
 			service.NewStringField(ddboFieldTable).
@@ -124,7 +124,7 @@ This output benefits from sending messages as a batch for improved performance. 
 					"full_content": "${!content()}",
 				}),
 			service.NewStringMapField(ddboFieldJSONMapColumns).
-				Description("A map of column keys to [field paths](/docs/configuration/field_paths) pointing to value data within messages.").
+				Description("A map of column keys to xref:configuration:field_paths.adoc[field paths] pointing to value data within messages.").
 				Default(map[string]any{}).
 				Example(map[string]any{
 					"user":           "path.to.user",

internal/impl/aws/output_kinesis_firehose.go
@@ -48,15 +48,15 @@ func kfoOutputSpec() *service.ConfigSpec {
 		Categories("Services", "AWS").
 		Summary(`Sends messages to a Kinesis Firehose delivery stream.`).
 		Description(`
-### Credentials
+== Credentials
 
-By default Bento will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more [in this document](/docs/guides/cloud/aws).
+By default Bento will use a shared credentials file when connecting to AWS services. It's also possible to set them explicitly at the component level, allowing you to transfer data across accounts. You can find out more in xref:guides:cloud/aws.adoc[].
 
-## Performance
+== Performance
 
 This output benefits from sending multiple messages in flight in parallel for improved performance. You can tune the max number of in flight messages (or message batches) with the field `+"`max_in_flight`"+`.
 
-This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more [in this doc](/docs/configuration/batching).
+This output benefits from sending messages as a batch for improved performance. Batches can be formed at both the input and output level. You can find out more xref:configuration:batching.adoc[in this doc].
 `).
 		Fields(
 			service.NewStringField(kfoFieldStream).

internal/impl/cockroachdb/input_changefeed.go
@@ -30,8 +30,8 @@ var sampleString = `{
 func crdbChangefeedInputConfig() *service.ConfigSpec {
 	return service.NewConfigSpec().
 		Categories("Integration").
-		Summary(fmt.Sprintf("Listens to a [CockroachDB Core Changefeed](https://www.cockroachlabs.com/docs/stable/changefeed-examples) and creates a message for each row received. Each message is a json object looking like: \n```json\n%s\n```", sampleString)).
-		Description("This input will continue to listen to the changefeed until shutdown. A backfill of the full current state of the table will be delivered upon each run unless a cache is configured for storing cursor timestamps, as this is how Bento keeps track as to which changes have been successfully delivered.\n\nNote: You must have `SET CLUSTER SETTING kv.rangefeed.enabled = true;` on your CRDB cluster, for more information refer to [the official CockroachDB documentation.](https://www.cockroachlabs.com/docs/stable/changefeed-examples?filters=core)").
+		Summary(fmt.Sprintf("Listens to a https://www.cockroachlabs.com/docs/stable/changefeed-examples[CockroachDB Core Changefeed] and creates a message for each row received. Each message is a json object looking like: \n```json\n%s\n```", sampleString)).
+		Description("This input will continue to listen to the changefeed until shutdown. A backfill of the full current state of the table will be delivered upon each run unless a cache is configured for storing cursor timestamps, as this is how Bento keeps track as to which changes have been successfully delivered.\n\nNote: You must have `SET CLUSTER SETTING kv.rangefeed.enabled = true;` on your CRDB cluster, for more information refer to https://www.cockroachlabs.com/docs/stable/changefeed-examples?filters=core[the official CockroachDB documentation].").
 		Fields(
 			service.NewStringField("dsn").
 				Description(`A Data Source Name to identify the target database.`).

internal/impl/pure/processor_metric.go
@@ -31,18 +31,18 @@ func metProcSpec() *service.ConfigSpec {
 		Stable().
 		Summary("Emit custom metrics by extracting values from messages.").
 		Description(`
-This processor works by evaluating an [interpolated field `+"`value`"+`](/docs/configuration/interpolation#bloblang-queries) for each message and updating a emitted metric according to the [type](#types).
+This processor works by evaluating an xref:configuration:interpolation.adoc#bloblang-queries[interpolated field `+"`value`"+`] for each message and updating a emitted metric according to the <<types, type>>.
 
-Custom metrics such as these are emitted along with Bento internal metrics, where you can customize where metrics are sent, which metric names are emitted and rename them as/when appropriate. For more information check out the [metrics docs here](/docs/components/metrics/about).`).
+Custom metrics such as these are emitted along with Bento internal metrics, where you can customize where metrics are sent, which metric names are emitted and rename them as/when appropriate. For more information see the xref:components:metrics/about.adoc[metrics docs].`).
 		Footnotes(`
-## Types
+== Types
 
-### `+"`counter`"+`
+=== `+"`counter`"+`
 
 Increments a counter by exactly 1, the contents of `+"`value`"+` are ignored
 by this type.
 
-### `+"`counter_by`"+`
+=== `+"`counter_by`"+`
 
 If the contents of `+"`value`"+` can be parsed as a positive integer value
 then the counter is incremented by this value.
@@ -59,7 +59,7 @@ pipeline:
         value: ${!json("field.some.value")}
 `+"```"+`
 
-### `+"`gauge`"+`
+=== `+"`gauge`"+`
 
 If the contents of `+"`value`"+` can be parsed as a positive integer value
 then the gauge is set to this value.
@@ -76,7 +76,7 @@ pipeline:
         value: ${!json("field.some.value")}
 `+"```"+`
 
-### `+"`timing`"+`
+=== `+"`timing`"+`
 
 Equivalent to `+"`gauge`"+` where instead the metric is a timing. It is recommended that timing values are recorded in nanoseconds in order to be consistent with standard Bento timing metrics, as in some cases these values are automatically converted into other units such as when exporting timings as histograms with Prometheus metrics.`).
 		Example(
@@ -124,7 +124,7 @@ metrics:
 		).
 		Fields(
 			service.NewStringEnumField(metProcFieldType, "counter", "counter_by", "gauge", "timing").
-				Description("The metric [type](#types) to create."),
+				Description("The metric <<types, type>> to create."),
 			service.NewStringField(metProcFieldName).
 				Description("The name of the metric to create, this must be unique across all Bento components otherwise it will overwrite those other metrics."),
 			service.NewInterpolatedStringMapField(metProcFieldLabels).